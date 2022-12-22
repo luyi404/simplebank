c4a2d1b2e019a5f993e7ba9bedf257f1b5fa1079
luyi404 <370977893@qq.com>
2022-12-22 15:28:23 -0500

    [ftr]Create grpc server, but not implement the function yet.

--- Makefile
+++ Makefile
@@ -37,4 +37,7 @@ proto:
         --go-grpc_out=pb --go-grpc_opt=paths=source_relative \
         proto/*.proto
 
-.PHONY: postgres createdb dropdb migrateup migratedown sqlc test server mock migrateup1 migratedown1 proto
+evans:
+	evans --host localhost --port 9091 -r repl
+
+.PHONY: postgres createdb dropdb migrateup migratedown sqlc test server mock migrateup1 migratedown1 proto evans
--- main.go
+++ main.go
@@ -5,8 +5,13 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/luyi404/simplebank/api"
 	db "github.com/luyi404/simplebank/db/sqlc"
+	"github.com/luyi404/simplebank/gapi"
+	"github.com/luyi404/simplebank/pb"
 	"github.com/luyi404/simplebank/util"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"log"
+	"net"
 )
 
 func main() {
@@ -20,14 +25,38 @@ func main() {
 		log.Fatal("failed to connect to database: ", err)
 	}
 	store := db.NewStore(conn)
+	runGrpcServer(config, store)
+	//runGinServer(config, store)
+}
+
+func runGrpcServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("failed to create a server: ", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterSimpleBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("failed to listen: ", err)
+	}
+	log.Printf("server is listening at %s", config.GRPCServerAddress)
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot serve: ", err)
+	}
+}
+
+func runGinServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
 		log.Fatal("failed to create a server: ", err)
 	}
 
-	err = server.Start(config.ServerAddress)
+	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("failed to start server: ", err)
 	}
-
 }
